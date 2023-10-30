b831e5acc77e3aeccd490bbfe07bb933b6266590
adventure29 <105844589+adventure29@users.noreply.github.com>
2023-10-30 20:17:56 +0530

    Update source.py

--- adverse_events_data/source.py
+++ adverse_events_data/source.py
@@ -16,7 +16,7 @@ def source_dataframe(sourceURL):
 
 #sourceURL = 'https://api.fda.gov/drug/event.json?search=patient.drug.openfda.pharm_class_epc:"Thiazide+Diuretic"&limit=1"
 #read the content of source - open API
-	httpData = urlopen(sourceURL).read().decode('utf-8')
+httpData = urlopen(sourceURL).read().decode('utf-8')
 # convert into RDD
 rdd = spark.sparkContext.parallelize([httpData])
 
@@ -28,38 +28,7 @@ df.count()
 
 #step 3: Performing some transformation to view data into some structured form
 #Requirement: to extract specific fields related to adverse events
-
-
-#method to read content of open api file we read
-def read_json_nested(df):
-    #Step 1: Defining column as an empty list
-    column_list = []
-    # for loop starts to find type of column type of dataframe
-    print("for loop starts")
-    for column_name in df.schema.names:
-        # Checking column type is ArrayType
-        if isinstance(df.schema[column_name].dataType, ArrayType):
-            print("Inside isinstance loop of ArrayType: " + column_name)
-            df = df.withColumn(column_name, explode(column_name).alias(column_name))
-            column_list.append(column_name)
-
-        elif isinstance(df.schema[column_name].dataType, StructType):
-            print("Inside isinstance loop of StructType: " + column_name)
-            for field in df.schema[column_name].dataType.fields:
-                column_list.append(col(column_name + "." + field.name).alias(column_name + "_" + field.name))
-        else:
-            column_list.append(column_name)
-
-    # Selecting columns using column_list from dataframe: df
-    df = df.select(column_list)
-    return df
-
-
-
-read_nested_json_flag = True
-  ##While Loop Starts
-while read_nested_json_flag:
-  sourceURL = input("Provide the adverse event Source URL: ")
+put("Provide the adverse event Source URL: ")
   print("Reading Nested JSON File ... ")
   df = read_json_nested(df)
   read_nested_json_flag = False
